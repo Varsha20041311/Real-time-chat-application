5cbf67292e21c03e9b37c96e48379f9bcb290969
Varsha20041311 <varshabas2004@gmail.com>
2025-07-16 19:46:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import necessary modules
+const express = require("express");            // Express framework for creating the server
+const http = require("http");                  // HTTP module to create server instance
+const { Server } = require("socket.io");       // Importing Server class from socket.io
+const cors = require("cors");                  // CORS middleware to allow cross-origin requests
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Create an Express app
+const app = express();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Use CORS to allow requests from any origin
+app.use(cors());
+
+// Create an HTTP server using the Express app
+const server = http.createServer(app);
+
+// Create a new instance of socket.io and attach it to the server
+const io = new Server(server, {
+  cors: {
+    origin: "*",                // Allow any origin
+    methods: ["GET", "POST"],   // Allow only GET and POST requests
+  },
+});
+
+// Listen for new client connections
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);  // Log the new user's socket ID
+
+  // Listen for the 'send_message' event from clients
+  socket.on("send_message", (data) => {
+    // Emit the message to all connected clients
+    io.emit("receive_message", data);
+  });
+
+  // Handle user disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);  // Log the disconnected user's socket ID
+  });
+});
+
+// Start the server on port 5000
+server.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
